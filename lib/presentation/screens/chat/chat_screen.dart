499dcbd0bb6986188e098a3fecb737b7489c2290
zabdiel61 <jonathanalbayero203@homail.com>
2023-06-29 14:41:29 -0600

    [PANTALLA DE MENSAJES] * creacion de burbujas de chat

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL4MOg_mOEI3VfcaCaBqzcebKZM1ycuVmG5Q&usqp=CAU'),
+          ),
+        ),
+        title: const Text("Prima Claudia 😃"),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return const MyMessageBubble();
+              },
+            )),
+            Text("Hola")
+          ],
+        ),
+      ),
+    );
+  }
+}
